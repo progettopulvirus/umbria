5c1bc48030b25a12b4ba3b78e48d8c9fb32659d6
guidofioravanti <guidofioravanti@yahoo.it>
2021-02-03 21:51:23 +0100

    Integratidati mancanti del PM10, caricati dati del benzene

---
+++ R/c6h6.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di c6h6 per 2 stazioni della regione UMBRIA
+#'
+#' @format Un tibble con 8 colonne e 5467 osservazioni
+#'
+#' @usage
+#' c6h6
+"c6h6"
---
+++ R/pm10.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di pm10 per 13 stazioni della regione UMBRIA
+#'
+#' @format Un tibble con 8 colonne e 35095 osservazioni
+#'
+#' @usage
+#' pm10
+"pm10"
